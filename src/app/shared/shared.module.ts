96e7830e978ae9b7475163d0d95d7dc138b21152
krzyzowskabeata <krzyzowska.beata@gmail.com>
2024-06-05 17:56:40 +0200

    Header as shared component using content projection

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
 
-const sharedComponents = [];
+const sharedComponents = [HeaderComponent];
 
 @NgModule({
-  declarations: [],
+  declarations: [...sharedComponents],
   imports: [CommonModule],
-  exports: []
+  exports: [...sharedComponents]
 })
 export class SharedModule { }
